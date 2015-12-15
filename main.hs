d32ea25e2ae533437c57544ed35d492ccc6056bb
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-12-15 21:57:24 +0900

    need to change file name for GHC to find it

--- main.hs
+++ main.hs
@@ -1,4 +1,8 @@
 import Puzzle (Puzzle, fromFile)
+import SinglesSolver (solve)
 
 testFile :: IO Puzzle
 testFile = fromFile "test.txt"
+
+solvedTestFile :: IO Puzzle
+solvedTestFile = testFile >>= return . solve
