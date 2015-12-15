64974465257fe8ebeed2844117e1b44bcd2098ca
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-12-15 13:23:07 +0900

    do not import Space (unused)

--- solver.hs
+++ solver.hs
@@ -2,7 +2,7 @@ module Solver (
   solve
 ) where
 
-import Puzzle (Space, Puzzle)
+import Puzzle (Puzzle)
 
 -- Class of functions that try to solve sudoku puzzles.
 -- If the puzzle cannot be solved, the solver should return Nothing.
