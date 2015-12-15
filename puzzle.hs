d735d28735961afa86045df43ca481ae37812f33
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-12-15 12:38:14 +0900

    functions for making puzzles, getting rows and columns

--- puzzle.hs
+++ puzzle.hs
@@ -1,5 +1,20 @@
 import Data.Array
 
-data Space = Given Int | Empty
+type Space = Maybe Int
 
 type Puzzle = Array (Int, Int) Space
+
+-- Creates a 9x9 sudoku puzzle from a list of spaces. The spaces should be
+-- indexed by row and then column, i.e. [(1, 1), (1, 2), ... (2, 1), ...].
+makePuzzle :: [Space] -> Puzzle
+makePuzzle = listArray ((1, 1), (9, 9))
+
+-- Returns the contents of a given row of a puzzle.
+row :: Int -> Puzzle -> Array Int Space
+row rowIndex puzzle =
+  array (1, 9) [(colIndex, puzzle ! (rowIndex, colIndex)) | colIndex <- [1..9]]
+
+-- Returns the contents of a given column of a puzzle.
+col :: Int -> Puzzle -> Array Int Space
+col colIndex puzzle =
+  array (1, 9) [(rowIndex, puzzle ! (rowIndex, colIndex)) | rowIndex <- [1..9]]
